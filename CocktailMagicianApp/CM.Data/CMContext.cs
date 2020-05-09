ec96518be0791cb12b0842e7b374f06dee2af79b
Angel Nikolov <ang.n.nikolov@gmail.com>
2020-05-09 19:24:25 +0300

    App skeleton upload

---
+++ CocktailMagicianApp/CM.Data/CMContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CM.Data
+{
+    public class CMContext : IdentityDbContext
+    {
+        public CMContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Team6_CocktailMagician
 
 Web app for browsing bars and the cocktails they offer.
+
+ [Find Trello board here!](https://trello.com/b/zMhsHErO/cocktail-magician)
