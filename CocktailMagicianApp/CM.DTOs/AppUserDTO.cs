3a2bfb7fecfe6e87b9feac949d4d96f29b37318f
MariaDaneva <georgieva.maria@ymail.com>
2020-05-11 01:14:10 +0300

    AppUserDTO implemented

---
+++ CocktailMagicianApp/CM.DTOs/AppUserDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CM.Models;
+
+namespace CM.DTOs
+{
+	public class AppUserDTO
+	{
+		public Guid Id { get; set; }
+		public string Username { get; set; }
+
+		public ICollection<BarRatingDTO> BarRatings { get; set; }
+		public ICollection<BarCommentDTO> BarComments { get; set; }
+
+		public ICollection<CocktailRatingDTO> CoctailRatings { get; set; }
+		public ICollection<CocktailCommentDTO> CocktailComments { get; set; }
+
+		public DateTime? DeletedOn { get; set; }
+		public bool IsDeleted { get; set; }
+
+		//TODO image
+	}
+}
