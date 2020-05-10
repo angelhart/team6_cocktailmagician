62716de64f6cbb72912d027b0173275d82aff898
MariaDaneva <georgieva.maria@ymail.com>
2020-05-10 22:59:14 +0300

    Bar implemented.

---
+++ CocktailMagicianApp/CM.Models/Bar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CM.Models
+{
+	public class Bar
+	{
+        public Bar()
+        {
+            Comments = new List<CocktailComment>();
+            Ratings = new List<CocktailRating>();
+            Cocktails = new List<BarCocktails>();
+        }
+        public Guid Id { get; set; }
+        [Required]
+        [MinLength(1)]
+        [MaxLength(256)]
+        public string Name { get; set; }
+        public Address Address { get; set; }
+        public bool IsUnlisted { get; set; }
+        public DateTime? DeletedOn { get; set; }
+        public bool IsDeleted { get; set; }
+        public ICollection<CocktailComment> Comments { get; set; }
+        public ICollection<CocktailRating> Ratings { get; set; }
+        public ICollection<BarCocktails> Cocktails { get; set; }
+        //TODO Image?
+    }
+}
