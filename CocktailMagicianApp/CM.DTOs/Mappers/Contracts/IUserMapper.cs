c10dcc6ce7793d7bd00b8e4e9a5adaa73d35ee09
MariaDaneva <georgieva.maria@ymail.com>
2020-05-11 01:15:28 +0300

    UserMapper implemented.

---
+++ CocktailMagicianApp/CM.DTOs/Mappers/Contracts/IUserMapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CM.Models;
+
+namespace CM.DTOs.Mappers.Contracts
+{
+    public interface IUserMapper
+    {
+        AppUserDTO CreateAppUserDTO(AppUser user);
+    }
+}
---
+++ CocktailMagicianApp/CM.DTOs/Mappers/UserMapper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CM.DTOs.Mappers.Contracts;
+using CM.Models;
+
+namespace CM.DTOs.Mappers
+{
+
+    public class UserMapper : IUserMapper
+    {
+        private readonly IBarMapper _barMapper;
+        private readonly ICocktailMapper _cocktailMapper;
+        public UserMapper(IBarMapper barMapper, ICocktailMapper cocktailMapper)
+        {
+            this._barMapper = barMapper ?? throw new ArgumentNullException(nameof(barMapper));
+            this._cocktailMapper = cocktailMapper ?? throw new ArgumentNullException(nameof(cocktailMapper));
+        }
+        public AppUserDTO CreateAppUserDTO(AppUser user)
+        {
+            return new AppUserDTO
+            {
+
+                //TODO image
+                Id = user.Id,
+                IsDeleted = user.IsDeleted,
+
+                BarRatings = user.BarRatings
+                            .Select(rating => _barMapper.CreateBarRatingDTO(rating)).ToList(),
+                BarComments = user.BarComments
+                            .Select(comment => _barMapper.CreateBarCommentDTO(comment)).ToList(),
+
+                CoctailRatings = user.CoctailRatings
+                            .Select(rating => _cocktailMapper.CreateCocktailRatingDTO(rating)).ToList(),
+                CocktailComments = user.CocktailComments
+                            .Select(comment => _cocktailMapper.CreateCocktailCommentDTO(comment)).ToList()
+            };
+        }
+    }
+}
