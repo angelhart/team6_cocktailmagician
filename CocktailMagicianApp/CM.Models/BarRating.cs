8434bcae03dac728c6e63a6c7077c3c4dc736026
MariaDaneva <georgieva.maria@ymail.com>
2020-05-10 23:00:52 +0300

    BarRating implemented.

---
+++ CocktailMagicianApp/CM.Models/BarRating.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CM.Models.BaseClasses;
+
+namespace CM.Models
+{
+	public class BarRating: Rating
+	{
+		public Guid BarId { get; set; }
+		public Bar Bar { get; set; }
+	}
+}
