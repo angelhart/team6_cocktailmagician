75c2930a1716c3ce100da09a65576d87c6ae21f8
MariaDaneva <georgieva.maria@ymail.com>
2020-05-10 22:59:32 +0300

    BarCocktails implemented.

--- CocktailMagicianApp/CM.Models/BarCocktails.cs
+++ CocktailMagicianApp/CM.Models/BarCocktails.cs
@@ -1,6 +1,12 @@
-﻿namespace CM.Models
+﻿using System;
+
+namespace CM.Models
 {
     public class BarCocktails
     {
+        public Guid BarId { get; set; }
+        public Bar Bar { get; set; }
+        public Guid CocktailId { get; set; }
+        public Cocktail Cocktail { get; set; }
     }
 }
