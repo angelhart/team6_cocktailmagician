613d92255d816154a0788dfffc0183f07ae3aa87
MariaDaneva <georgieva.maria@ymail.com>
2020-05-10 23:01:52 +0300

    Country class implemented.

---
+++ CocktailMagicianApp/CM.Models/Country.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CM.Models
+{
+	public class Country
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+
+		public ICollection<Address> Adresses { get; set; }
+}
+}
