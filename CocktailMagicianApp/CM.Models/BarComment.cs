b57dc683e114ade5f970f2b9d91f55d404272b33
MariaDaneva <georgieva.maria@ymail.com>
2020-05-10 22:59:47 +0300

    BarComment implemented

---
+++ CocktailMagicianApp/CM.Models/BarComment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using CM.Models.BaseClasses;
+
+namespace CM.Models
+{
+	public class BarComment: Comment
+    {
+        //public Guid Id { get; set; }
+        public Guid BarId { get; set; }
+        public Bar Bar { get; set; }
+    }
+}
