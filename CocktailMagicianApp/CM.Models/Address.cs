a3e61b7129165f49a388ee326166b27d47d4d8c3
MariaDaneva <georgieva.maria@ymail.com>
2020-05-10 20:37:27 +0300

    Address model implemented

---
+++ CocktailMagicianApp/CM.Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CM.Models
+{
+	public class Address
+	{
+        public Guid Id { get; set; }
+        public Guid CountryId { get; set; }
+        public Country Country { get; set; }
+        public string City { get; set; }
+        public Guid BarId { get; set; }
+        public Bar Bar { get; set; }
+    }
+}
