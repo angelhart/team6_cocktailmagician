5e366c7a385d31303bff5ca96b6dae97d02e115b
MariaDaneva <georgieva.maria@ymail.com>
2020-05-11 01:14:52 +0300

    BarRatingDTO implemented.

---
+++ CocktailMagicianApp/CM.DTOs/BarRatingDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CM.DTOs
+{
+	public class BarRatingDTO
+	{
+		public Guid BarId { get; set; }
+		public string BarName { get; set; }
+		public int Score { get; set; }
+		public Guid AppUserId { get; set; }
+		public string AppUserName { get; set; }
+	}
+}
