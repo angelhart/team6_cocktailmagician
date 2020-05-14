354ae5c0b354882d9537e562e707456bc55ad0c9
MariaDaneva <georgieva.maria@ymail.com>
2020-05-14 20:00:41 +0300

    Some documentation to BarServices

--- CocktailMagicianApp/CM.Services/BarServices.cs
+++ CocktailMagicianApp/CM.Services/BarServices.cs
@@ -1,44 +1,72 @@
-﻿using CM.DTOs;
+﻿using CM.Data;
+using CM.DTOs;
+using CM.DTOs.Mappers.Contracts;
+using CM.Models;
 using CM.Services.Contracts;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CM.Services
 {
     public class BarServices : IBarServices
     {
-        public BarDTO AddReview(int id, BarCommentDTO barCommentDTO)
+        private readonly CMContext _context;
+        private readonly IBarMapper _barMapper;
+
+        public BarServices(CMContext context, IBarMapper barMapper)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _barMapper = barMapper ?? throw new ArgumentNullException(nameof(barMapper));
+        }
+
+
+        /// <summary>
+        /// Retrieves a collection of all bars in the database.
+        /// </summary>
+        /// <returns>ICollection</returns>
+        public async Task<ICollection<BarDTO>> GetAllBars()
         {
             throw new NotImplementedException();
         }
 
-        public BarDTO CreateBeer(BarDTO barDTO)
+        /// <summary>
+        /// Retrieves a specific bar by given Id.
+        /// </summary>
+        /// <param name="id">Guid representing Id.</param>
+        /// <returns>BarDTO</returns>
+        public async Task<BarDTO> GetBar(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Updates the current state of a bar.
+        /// </summary>
+        /// <param name="id">Guid representing Id.</param>
+        /// <param name="barDTO">The Bar that should be updated.</param>
+        /// <returns></returns>
+        public async Task<BarDTO> UpdateBar(Guid id, BarDTO barDTO)
         {
             throw new NotImplementedException();
         }
 
-        public BarDTO DeleteBeer(int id)
+        /// <summary>
+        /// Creates a new Bar.
+        /// </summary>
+        /// <param name="barDTO">The params needed to create a bar. </param>
+        /// <returns>BarDTO</returns>
+        public async Task<BarDTO> CreateBar(BarDTO barDTO)
         {
             throw new NotImplementedException();
         }
 
-        public ICollection<BarDTO> GetAllBars()
+        private async Task<IQueryable<Bar>> SearchBarsAsync(string searchString)
         {
             throw new NotImplementedException();
         }
-
-        public BarDTO GetBar(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public BarDTO RateBeer(int id, BarRatingDTO barRatingDTO)
-        {
-            throw new NotImplementedException();
-        }
-
-        public BarDTO UpdateBeer(int id, BarDTO barDTO)
+        private async Task<IQueryable<Bar>> SortBarsAsync(string sortBy, string sortOrder)
         {
             throw new NotImplementedException();
         }
--- CocktailMagicianApp/CM.Services/Contracts/IBarServices.cs
+++ CocktailMagicianApp/CM.Services/Contracts/IBarServices.cs
@@ -1,19 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using CM.DTOs;
 
 namespace CM.Services.Contracts
 {
     public interface IBarServices
     {
-		BarDTO GetBar(int id);
-		ICollection<BarDTO> GetAllBars();
-		BarDTO CreateBeer(BarDTO barDTO);
-		BarDTO UpdateBeer(int id, BarDTO barDTO);
-		BarDTO DeleteBeer(int id);
-		BarDTO RateBeer(int id, BarRatingDTO barRatingDTO);
-		BarDTO AddReview(int id, BarCommentDTO barCommentDTO);
+		public  Task<BarDTO> GetBar(Guid id);
+		public  Task<ICollection<BarDTO>> GetAllBars();
+		public  Task<BarDTO> CreateBar(BarDTO barDTO);
+		public  Task<BarDTO> UpdateBar(Guid id, BarDTO barDTO);
 
 		//TODO sorting
 		//TODO paging
