2d41e37244b68f9fc3d6e40a0dddcb950cbda484
MariaDaneva <georgieva.maria@ymail.com>
2020-05-17 16:42:36 +0300

    AddressDTO implemented.

---
+++ CocktailMagicianApp/CM.DTOs/AddressDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CM.Models;
+
+namespace CM.DTOs
+{
+	public class AddressDTO
+	{
+        public Guid Id { get; set; }
+
+        public Guid CityId { get; set; }
+        public CityDTO City { get; set; }
+
+        public string Street { get; set; }
+        public string CountryName { get; set; }
+    }
+}
