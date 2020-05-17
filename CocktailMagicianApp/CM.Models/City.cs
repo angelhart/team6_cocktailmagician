c3085e4006dc5b184b06f8da109baabcd2956ba5
MariaDaneva <georgieva.maria@ymail.com>
2020-05-17 16:43:14 +0300

    City model and CityDTO implemented

---
+++ CocktailMagicianApp/CM.Models/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CM.Models
+{
+	public class City
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public Guid CountryId { get; set; }
+		public Country Country { get; set; }
+
+		public ICollection<Address> Adresses { get; set; }
+	}
+}
