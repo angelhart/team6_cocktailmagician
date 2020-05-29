fe8360d1173030edbe3190609a4298f9bb168048
Angel Nikolov <ang.n.nikolov@gmail.com>
2020-05-29 13:15:16 +0300

    Custom attributes added

---
+++ CocktailMagicianApp/CM.Web/Providers/CustomAttributes/AllowedExtensionsAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+
+namespace CM.Web.Providers.CustomAttributes
+{
+    public class AllowedExtensionsAttribute : ValidationAttribute
+    {
+        /// <summary>
+        /// Initializes new instance of <see cref="AllowedExtensionsAttribute"/> class.
+        /// </summary>
+        /// <param name="AllowedExtensions">Allowed extensions represented as string array.</param>
+        public AllowedExtensionsAttribute(string[] extensions)
+        {
+            AllowedExtensions = extensions;
+        }
+
+        public string[] AllowedExtensions { get; }
+
+        public string GetErrorMessage() =>
+            $"Incorrect file type! Allowed extensions are {String.Join(", ", AllowedExtensions)}!";
+
+        public override bool IsValid(object value)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                var extension = Path.GetExtension(file.FileName);
+                return AllowedExtensions.Contains(extension, StringComparer.InvariantCultureIgnoreCase);                
+            }
+
+            return false;
+        }
+    }
+}
