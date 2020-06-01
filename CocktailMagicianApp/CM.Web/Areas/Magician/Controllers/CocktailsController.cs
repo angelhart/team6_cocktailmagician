298c63b86191f3ef0ae53202b3eec13cdcdea8dd
Angel Nikolov <ang.n.nikolov@gmail.com>
2020-06-01 23:33:28 +0300

    Scafolded cocktail views

--- CocktailMagicianApp/CM.Web/Areas/Magician/Controllers/CocktailsController.cs
+++ CocktailMagicianApp/CM.Web/Areas/Magician/Controllers/CocktailsController.cs
@@ -2,106 +2,183 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using CM.DTOs.Mappers.Contracts;
-using CM.Services.Contracts;
-using CM.Web.Providers.Contracts;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CM.Data;
+using CM.Models;
+using CM.Services.Contracts;
+using CM.DTOs.Mappers.Contracts;
+using Microsoft.AspNetCore.Hosting;
+using CM.Web.Providers.Contracts;
+using NToastNotify;
 
 namespace CM.Web.Areas.Magician.Controllers
 {
+    [Area("Magician")]
     public class CocktailsController : Controller
     {
         private readonly ICocktailServices _cocktailServices;
         private readonly ICocktailMapper _cocktailMapper;
+        private readonly ICocktailViewMapper _cocktailViewMapper;
         private readonly IWebHostEnvironment _webHostEnvironment;
         private readonly IStorageProvider _storageProvider;
+        private readonly IRatingServices _ratingServices;
+        private readonly IToastNotification _toastNotification;
+        private readonly ICommentServices _commentServices;
 
         public CocktailsController(ICocktailServices cocktailServices,
                                    ICocktailMapper cocktailMapper,
+                                   ICocktailViewMapper cocktailViewMapper,
                                    IWebHostEnvironment webHostEnvironment,
-                                   IStorageProvider storageProvider)
+                                   IStorageProvider storageProvider,
+                                   IRatingServices ratingServices,
+                                   IToastNotification toastNotification,
+                                   ICommentServices commentServices)
         {
             this._cocktailServices = cocktailServices;
             this._cocktailMapper = cocktailMapper;
+            this._cocktailViewMapper = cocktailViewMapper;
             this._webHostEnvironment = webHostEnvironment;
             this._storageProvider = storageProvider;
+            this._ratingServices = ratingServices;
+            this._toastNotification = toastNotification;
+            this._commentServices = commentServices;
         }
 
-        // GET: CocktailsController
-        public ActionResult Index()
+        // GET: Magician/Cocktails
+        public async Task<IActionResult> Index()
+        {
+            var dtos = await _cocktailServices.PageCocktailsAsync();
+            var vms = dtos.Select(c => _cocktailViewMapper.CreateCocktailViewModel(c));
+
+            return View(vms);
+        }
+
+        // GET: Magician/Cocktails/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cocktail = await _context.Cocktails
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cocktail == null)
+            {
+                return NotFound();
+            }
+
+            return View(cocktail);
+        }
+
+        // GET: Magician/Cocktails/Create
+        public IActionResult Create()
         {
             return View();
         }
 
-        // GET: CocktailsController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: CocktailsController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: CocktailsController/Create
+        // POST: Magician/Cocktails/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<IActionResult> Create([Bind("Id,Name,Recipe,ImagePath,IsUnlisted,AverageRating")] Cocktail cocktail)
         {
-            try
+            if (ModelState.IsValid)
             {
+                cocktail.Id = Guid.NewGuid();
+                _context.Add(cocktail);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            return View(cocktail);
         }
 
-        // GET: CocktailsController/Edit/5
-        public ActionResult Edit(int id)
+        // GET: Magician/Cocktails/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cocktail = await _context.Cocktails.FindAsync(id);
+            if (cocktail == null)
+            {
+                return NotFound();
+            }
+            return View(cocktail);
         }
 
-        // POST: CocktailsController/Edit/5
+        // POST: Magician/Cocktails/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,Recipe,ImagePath,IsUnlisted,AverageRating")] Cocktail cocktail)
         {
-            try
+            if (id != cocktail.Id)
             {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cocktail);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CocktailExists(cocktail.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            return View(cocktail);
         }
 
-        // GET: CocktailsController/Delete/5
-        public ActionResult Delete(int id)
+        // GET: Magician/Cocktails/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cocktail = await _context.Cocktails
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cocktail == null)
+            {
+                return NotFound();
+            }
+
+            return View(cocktail);
         }
 
-        // POST: CocktailsController/Delete/5
-        [HttpPost]
+        // POST: Magician/Cocktails/Delete/5
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            var cocktail = await _context.Cocktails.FindAsync(id);
+            _context.Cocktails.Remove(cocktail);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CocktailExists(Guid id)
+        {
+            return _context.Cocktails.Any(e => e.Id == id);
         }
     }
 }
