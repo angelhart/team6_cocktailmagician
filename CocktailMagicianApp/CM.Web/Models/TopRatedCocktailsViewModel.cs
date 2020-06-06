b97f5ddade8e0243f798a5836404d8f904c73dbe
MariaDaneva <georgieva.maria@ymail.com>
2020-06-06 19:22:47 +0300

    Added links to Top rated bars and cocktails

--- CocktailMagicianApp/CM.Web/Models/TopRatedCocktailsViewModel.cs
+++ CocktailMagicianApp/CM.Web/Models/TopRatedCocktailsViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace CM.Web.Models
+﻿using System;
+
+namespace CM.Web.Models
 {
 	public class TopRatedCocktailsViewModel
 	{
+		public Guid Id { get; set; }
 		public string Name { get; set; }
 		public string ImagePath { get; set; }
 		public double? AverageRating { get; set; }
