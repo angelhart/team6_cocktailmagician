ad03eda0ed262589c84d23726cb4f8326b141953
MariaDaneva <georgieva.maria@ymail.com>
2020-06-10 17:03:32 +0300

    Some More Tests added

--- CocktailMagicianApp/CM.ServicesTests/BarServicesTests/GetAllBarsAsync_Should.cs
+++ CocktailMagicianApp/CM.ServicesTests/BarServicesTests/GetAllBarsAsync_Should.cs
@@ -1,20 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using CM.Data;
+using CM.DTOs;
+using CM.DTOs.Mappers.Contracts;
+using CM.Models;
+using CM.Services;
+using CM.Services.Contracts;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace CM.ServicesTests.BarServicesTests
 {
 	[TestClass]
 	public class GetAllBarsAsync_Should
 	{
-		//public async Task Return_SortedPaginatedListByRating_IfPassedSortCriteria_IsRating() { }
-		//public async Task Return_SortedPaginatedListByName_IfPassedSortCriteria_IsNull() { }
-		//public async Task Return_SortedPaginatedListByNameDesc_IfPassedSortCriteria_IsNameDesc() { }
-		//public async Task Return_SortedPaginatedListByRatingDes_IfPassedSortCriteria_IsRating_asc() { }
-		//public async Task Return_PaginatedList_WithThreeBars_PerPage() { }
-		//public async Task Return_TheRestOfTheBars_IfThereAreNoTwoBarsLeft() { }
-		//public async Task Return_OnlyTheBarsWichIsNotDeleted() { }
-		//public async Task Return_PaginatedListWithBarDtos_WhenValidValuesPassed() { }
-	}
+        [TestMethod]
+        public async Task ReturnWithoutUnlisted()
+        {
+            var options = Utility.GetOptions(nameof(ReturnWithoutUnlisted));
+            await Utility.ArrangeContextAsync(options);
+
+            var mockAddressServices = new Mock<IAddressServices>();
+            var mockMapper = new Mock<IBarMapper>();
+            mockMapper.Setup(x => x.CreateBarDTO(It.IsAny<Bar>()))
+                          .Returns<Bar>(bar => new BarDTO
+                          {
+                              Id = bar.Id,
+                              Name = bar.Name,
+                              IsUnlisted = bar.IsUnlisted
+                          });
+
+            var pageNumber = 1;
+            var pageSize = 1;
+
+            using var assertContext = new CMContext(options);
+            {
+                var sut = new BarServices(assertContext, mockMapper.Object, mockAddressServices.Object);
+                var result = await sut.GetAllBarsAsync(pageNumber: pageNumber, pageSize: pageSize, allowUnlisted: false);
+                Assert.AreEqual(pageSize, result.Count);
+                Assert.AreEqual("Bar A", result[0].Name);
+            }
+        }
+
+        [TestMethod]
+        public async Task ReturnWithUnlisted()
+        {
+            var options = Utility.GetOptions(nameof(ReturnWithUnlisted));
+            await Utility.ArrangeContextAsync(options);
+
+            var mockAddressServices = new Mock<IAddressServices>();
+            var mockMapper = new Mock<IBarMapper>();
+            mockMapper.Setup(x => x.CreateBarDTO(It.IsAny<Bar>()))
+                          .Returns<Bar>(bar => new BarDTO
+                          {
+                              Id = bar.Id,
+                              Name = bar.Name,
+                              IsUnlisted = bar.IsUnlisted
+                          });
+
+            var pageNumber = 1; 
+            var pageSize = 10; 
+
+            using var assertContext = new CMContext(options);
+            {
+                var sut = new BarServices(assertContext, mockMapper.Object, mockAddressServices.Object);
+                var result = await sut.GetAllBarsAsync(pageNumber: pageNumber, pageSize: pageSize, allowUnlisted: true);
+                Assert.IsTrue(result.Any(b => b.IsUnlisted == true));
+            }
+        }
+    }
 }
